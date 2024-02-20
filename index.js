7c62ef4ecb7c62d64d114ee1fde1c2c2d84e97e7
follow-to-submit-appeal <159720493+follow-to-submit-appeal@users.noreply.github.com>
2024-02-20 13:24:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -31,22 +31,4 @@ function submitForm(event) {
   });
 }
 
-  const formData = new FormData(document.getElementById('myForm'));
-
-  fetch('https://submit-form.com/CN2jV0d3T', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      console.log('Form submitted successfully');
-      // Handle success, e.g., display a success message
-    } else {
-      console.error('Form submission failed');
-      // Handle errors, e.g., display an error message
-    }
-  })
-  .catch(error => {
-    console.error('An error occurred during form submission', error);
-  });
-}
+  
